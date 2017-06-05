15ba6e3c0ab8792812caac1b751cf57c211a8fe2
bitcraze <bitcraze@bitcraze-vm.unassigned-domain>
2017-06-05 19:09:04 +0200

    -

--- src/modules/src/stabilizer.c
+++ src/modules/src/stabilizer.c
@@ -64,10 +64,14 @@ extern uint16_t range_last2[6];
 #include "vl53l0x.h"
 static point_t position;
 static point_t estimate;
+static point_t estimate_old;
+static point_t vel;
 static float kp = 0.002f;
+static float kd = 0.002f;
 static float xC = 0.0f;
 static float yC = 0.0f;
 static float alpha = 0.93f;
+//static uint8_t makeCenter = 1;
 #define N 1
 
 static void stabilizerTask(void* param);
@@ -142,6 +146,11 @@ static void stabilizerTask(void* param)
 		commanderGetSetpoint(&setpoint, &state);
 
 		if (vl53l0xReadPosition(&position, tick)) {
+//			if (makeCenter==1) {
+//				xC = position.x;
+//				yC = position.y;
+//				makeCenter = 0;
+//			}
 			position.x += xC;
 			position.y += yC;
 		}
@@ -169,145 +178,23 @@ static void stabilizerTask(void* param)
 		if (!firstRun && (position.timestamp==tick)) {
 			estimate.x += (float) ((position.x - prevX[0])/N);
 			estimate.y += (float) ((position.y - prevY[0])/N);
+			vel.x = (float) (estimate.x - estimate_old.x)*1000.0f;
+			vel.y = (float) (estimate.y - estimate_old.y)*1000.0f;
 			for (int i=0;i<N-1;i++) {
 				prevX[i] = prevX[i+1];
 				prevY[i] = prevY[i+1];
 			}
 			prevX[N-1] = position.x;
 			prevY[N-1] = position.y;
+			estimate_old.x = estimate.x;
+			estimate_old.y = estimate.y;
 			//			estimate.x = alpha * estimate.x + (1.0f - alpha) * position.x;
 			//			estimate.y = alpha * estimate.y + (1.0f - alpha) * position.y;
 		}
 		if (1) {
-			setpoint.attitude.pitch = kp*(estimate.x);
-			setpoint.attitude.roll = kp*(-estimate.y);
+			setpoint.attitude.pitch = kp*(estimate.x) + kd*(vel.x);
+			setpoint.attitude.roll = kp*(-estimate.y) + kd*(vel.y);;
 		}
-		//		if (range_last2[3] != 0 && range_last2[3] < RANGE_OUTLIER_LIMIT) {
-		//			position.x += (float)range_last2[3]/2.0f;
-		//		}
-		//		if (range_last2[1] != 0 && range_last2[1] < RANGE_OUTLIER_LIMIT) {
-		//			position.x -= (float)range_last2[1]/2.0f;
-		//		}
-		//		if (range_last2[4] != 0 && range_last2[4] < RANGE_OUTLIER_LIMIT) {
-		//			position.y += (float)range_last2[4]/2.0f;
-		//		}
-		//		if (range_last2[2] != 0 && range_last2[2] < RANGE_OUTLIER_LIMIT) {
-		//			position.y -= (float)range_last2[2]/2.0f;
-		//		}
-
-		//		if (altHoldMode) {
-		//			setpoint.attitude.pitch = kp*(position.x-position.y);
-		//			setpoint.attitude.roll = kp*(-position.x-position.y);
-		//		}
-		//		if (nonLinear && !centerSensor) {
-		//			if (range_last2[1]<1000.0 && range_last2[1]>0.0) {
-		//				setpoint.attitude.roll -= (float)  (nonLinearConst*1000/range_last2[1]);
-		//				setpoint.attitude.pitch += (float) (nonLinearConst*1000/range_last2[1]);
-		//			}
-		//			//
-		//			if (range_last2[2]<1000.0 && range_last2[2]>0.0) {
-		//				setpoint.attitude.roll -= (float)  (nonLinearConst*1000/range_last2[2]);
-		//				setpoint.attitude.pitch -= (float) (nonLinearConst*1000/range_last2[2]);
-		//			}
-		//
-		//			if (range_last2[3]<1000.0 && range_last2[3]>0.0) {
-		//				setpoint.attitude.roll += (float)  (nonLinearConst*1000/range_last2[3]);
-		//				setpoint.attitude.pitch -= (float) (nonLinearConst*1000/range_last2[3]);
-		//			}
-		//
-		//			if (range_last2[4]<1000.0 && range_last2[4]>0.0) {
-		//				setpoint.attitude.roll += (float)  (nonLinearConst*1000/range_last2[4]);
-		//				setpoint.attitude.pitch += (float) (nonLinearConst*1000/range_last2[4]);
-		//			}
-		//		}
-		//
-		//		if (Linear && !centerSensor) {
-		//			if (range_last2[1]<1000.0 && range_last2[1]>0.0) {
-		//				setpoint.attitude.roll -= LinearConst*(1 - range_last2[1]/1000);
-		//				setpoint.attitude.pitch += LinearConst*(1 - range_last2[1]/1000);
-		//			}
-		//
-		//			if (range_last2[2]<1000.0 && range_last2[2]>0.0) {
-		//				setpoint.attitude.roll -= LinearConst*(1 - range_last2[2]/1000);
-		//				setpoint.attitude.pitch -= LinearConst*(1 - range_last2[2]/1000);
-		//			}
-		//
-		//			if (range_last2[3]<1000.0 && range_last2[3]>0.0) {
-		//				setpoint.attitude.roll += LinearConst*(1 - range_last2[3]/1000);
-		//				setpoint.attitude.pitch -= LinearConst*(1 - range_last2[3]/1000);
-		//			}
-		//
-		//			if (range_last2[4]<1000.0 && range_last2[4]>0.0) {
-		//				setpoint.attitude.roll += LinearConst*(1 - range_last2[4]/1000);
-		//				setpoint.attitude.pitch += LinearConst*(1 - range_last2[4]/1000);
-		//			}
-		//		}
-		//
-		//		if (nonLinear && centerSensor) {
-		//			if (range_last2[1]<1000.0 && range_last2[1]>0.0) {
-		////				setpoint.attitude.roll -= (float)  (nonLinearConst*1000/range_last2[1]);
-		//				setpoint.attitude.pitch += (float) (nonLinearConst*1000/range_last2[1]);
-		//			}
-		//			//
-		//			if (range_last2[2]<1000.0 && range_last2[2]>0.0) {
-		//				setpoint.attitude.roll -= (float)  (nonLinearConst*1000/range_last2[2]);
-		////				setpoint.attitude.pitch -= (float) (nonLinearConst*1000/range_last2[2]);
-		//			}
-		//
-		//			if (range_last2[3]<1000.0 && range_last2[3]>0.0) {
-		////				setpoint.attitude.roll += (float)  (nonLinearConst*1000/range_last2[3]);
-		//				setpoint.attitude.pitch -= (float) (nonLinearConst*1000/range_last2[3]);
-		//			}
-		//
-		//			if (range_last2[4]<1000.0 && range_last2[4]>0.0) {
-		//				setpoint.attitude.roll += (float)  (nonLinearConst*1000/range_last2[4]);
-		////				setpoint.attitude.pitch += (float) (nonLinearConst*1000/range_last2[4]);
-		//			}
-		//		}
-		//
-		//		if (Linear && centerSensor) {
-		//			if (range_last2[1]<1000.0 && range_last2[1]>0.0) {
-		////				setpoint.attitude.roll -= LinearConst*(1 - range_last2[1]/1000);
-		//				setpoint.attitude.pitch += LinearConst*(1 - range_last2[1]/1000);
-		//			}
-		//
-		//			if (range_last2[2]<1000.0 && range_last2[2]>0.0) {
-		//				setpoint.attitude.roll -= LinearConst*(1 - range_last2[2]/1000);
-		////				setpoint.attitude.pitch -= LinearConst*(1 - range_last2[2]/1000);
-		//			}
-		//
-		//			if (range_last2[3]<1000.0 && range_last2[3]>0.0) {
-		////				setpoint.attitude.roll += LinearConst*(1 - range_last2[3]/1000);
-		//				setpoint.attitude.pitch -= LinearConst*(1 - range_last2[3]/1000);
-		//			}
-		//
-		//			if (range_last2[4]<1000.0 && range_last2[4]>0.0) {
-		//				setpoint.attitude.roll += LinearConst*(1 - range_last2[4]/1000);
-		////				setpoint.attitude.pitch += LinearConst*(1 - range_last2[4]/1000);
-		//			}
-		//		}
-
-
-		//		if (range_last2[1]<1000.0 && range_last2[1]>0.0) {
-		//			//				setpoint.attitude.roll -= LinearConst*(1 - range_last2[1]/1000);
-		//			setpoint.attitude.pitch += LinearConst*(1 - range_last2[1]/1000);
-		//		}
-		//
-		//		if (range_last2[2]<1000.0 && range_last2[2]>0.0) {
-		//			setpoint.attitude.roll -= LinearConst*(1 - range_last2[2]/1000);
-		//			//				setpoint.attitude.pitch -= LinearConst*(1 - range_last2[2]/1000);
-		//		}
-		//
-		//		if (range_last2[3]<1000.0 && range_last2[3]>0.0) {
-		//			//				setpoint.attitude.roll += LinearConst*(1 - range_last2[3]/1000);
-		//			setpoint.attitude.pitch -= LinearConst*(1 - range_last2[3]/1000);
-		//		}
-		//
-		//		if (range_last2[4]<1000.0 && range_last2[4]>0.0) {
-		//			setpoint.attitude.roll += LinearConst*(1 - range_last2[4]/1000);
-		//			//				setpoint.attitude.pitch += LinearConst*(1 - range_last2[4]/1000);
-		//		}
-
 
 		sitAwUpdateSetpoint(&setpoint, &sensorData, &state);
 
@@ -364,6 +251,8 @@ LOG_ADD(LOG_FLOAT, y, &position.y)
 LOG_ADD(LOG_FLOAT, z, &position.z)
 LOG_ADD(LOG_FLOAT, estimateX, &estimate.x)
 LOG_ADD(LOG_FLOAT, estimateY, &estimate.y)
+LOG_ADD(LOG_FLOAT, velX, &vel.x)
+LOG_ADD(LOG_FLOAT, velY, &vel.y)
 LOG_ADD(LOG_UINT32, tick, &position.timestamp)
 LOG_GROUP_STOP(position)
 
@@ -389,7 +278,9 @@ PARAM_GROUP_START(posCtlPid)
 //PARAM_ADD(PARAM_UINT8, nonLinear, &nonLinear)
 //PARAM_ADD(PARAM_FLOAT, nonLinearConst, &nonLinearConst)
 PARAM_ADD(PARAM_FLOAT, kp, &kp)
+PARAM_ADD(PARAM_FLOAT, kd, &kd)
 PARAM_ADD(PARAM_FLOAT, xC, &xC)
 PARAM_ADD(PARAM_FLOAT, yC, &yC)
 PARAM_ADD(PARAM_FLOAT, alpha, &alpha)
+//PARAM_ADD(PARAM_UINT8, makeCenter, &makeCenter)
 PARAM_GROUP_STOP(posCtlPid)
