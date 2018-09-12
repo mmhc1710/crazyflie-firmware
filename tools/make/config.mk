1ee9cd8384c506206486cef96fe3bc71404be806
bitcraze <bitcraze@bitcraze-vm.unassigned-domain>
2018-09-12 21:07:56 +0200

    -

--- tools/make/config.mk
+++ tools/make/config.mk
@@ -8,7 +8,8 @@
 
 ## Load a deck driver that has no OW memory
 # CFLAGS += -DDECK_FORCE=bcBuzzer
-CFLAGS += -DDECK_FORCE=bcOA
+#CFLAGS += -DDECK_FORCE=bcOA
+CFLAGS += -DDECK_FORCE=bcDWM1000
 DEBUG = 1
 
 ## Load multiple deck drivers that has no OW memory
